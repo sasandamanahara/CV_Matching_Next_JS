6a438a3226f2c2ad99fd75f8d5b2dab38084290b
sasandamanahara <sasandamanahara12@gmail.com>
2024-03-29 21:20:36 +0530

    Message

--- src/app/(company)/messages/page.jsx
+++ src/app/(company)/messages/page.jsx
@@ -9,6 +9,7 @@ import {
 import { Button } from "@/components/ui/button"
 
 import CompanyHeader from "../_components/header"
+import Chat from "./_components/chat"
 
 export default function Messages() {
   return (
@@ -24,12 +25,8 @@ export default function Messages() {
           </div>
           <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
             <div className="flex flex-col items-center gap-1 text-center">
-              <h3 className="text-2xl font-bold tracking-tight">
-                
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                
-              </p>
+              
+              <Chat/>
               
             </div>
           </div>
