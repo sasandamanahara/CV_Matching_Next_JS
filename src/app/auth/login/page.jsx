60c975f0c623ecb7394a93e5af920e4fbfb649d4
sasandamanahara <sasandamanahara12@gmail.com>
2024-03-29 09:55:00 +0530

    AuthenticationTabs

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -13,9 +14,65 @@ import { Label } from "@/components/ui/label"
 
 export default function LoginForm() {
   return (
-    <Card className="mx-auto max-w-sm">
+<Card className="mx-auto max-w-sm">
+        <Tabs defaultValue="company" className=" mx-auto max-w-sm">
+        <TabsList >
+            <TabsTrigger  value="company">Company</TabsTrigger>
+            <TabsTrigger value="jobseeker">JobSeeker</TabsTrigger>
+        </TabsList>
+        <TabsContent value="company">
+
+        <Card className="mx-auto max-w-sm">
       <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
+        <CardTitle className="text-2xl">Company Login</CardTitle>
+        <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <div className="flex items-center">
+              <Label htmlFor="password">Password</Label>
+              <Link href="#" className="ml-auto inline-block text-sm underline">
+                Forgot your password?
+              </Link>
+            </div>
+            <Input id="password" type="password" required />
+          </div>
+          <Button type="submit" className="w-full">
+            Login 
+          </Button>
+          <Button variant="outline" className="w-full">
+            Login with Google
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Don&apos;t have an account?{" "}
+          <Link href="#" className="underline">
+            Sign up
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  
+
+        </TabsContent>
+        <TabsContent value="jobseeker">
+
+
+        <Card className="mx-auto max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">Job Seeker Login</CardTitle>
         <CardDescription>
           Enter your email below to login to your account
         </CardDescription>
@@ -55,5 +112,11 @@ export default function LoginForm() {
         </div>
       </CardContent>
     </Card>
+  
+        </TabsContent>
+        </Tabs>
+        </Card>
+
   )
+   
 }
