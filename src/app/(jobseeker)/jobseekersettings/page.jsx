a5ec7193b91351bbbeba3e1d5428342c7b90f1aa
Sanduni Manjula <sandunimanju.27@gmail.com>
2024-04-12 22:42:22 +0530

    setting-job seeker

--- src/app/(jobseeker)/jobseekersettings/page.jsx
+++ src/app/(jobseeker)/jobseekersettings/page.jsx
@@ -1,6 +1,10 @@
 
 
 import Profile from "./components/profile"
+import Logindetails from "./components/security"
+import Notification from "./components/notifications"
+import SideBar from "../_components/sidebar"
+import CompanyHeader from "../_components/header"
 import {
   Card,
   CardContent,
@@ -19,11 +23,26 @@ import {
 
 export default function TabsDemo() {
   return (
-    <Tabs defaultValue="profile" className="w-[900px]">
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="hidden border-r bg-muted/40 md:block">
+      <SideBar/>
+    </div>
+    <div className="flex flex-col">
+      <CompanyHeader/>
+      <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <div className="flex items-center">
+          <h1 className="text-lg font-semibold md:text-2xl">Settings</h1>
+          </div>
+        <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+          <div className="flex flex-col items-center gap-1 text-center">
+            
+            
+          </div>
+          <Tabs defaultValue="profile" className="w-[900px]">
       <TabsList className="grid w-full grid-cols-3">
         <TabsTrigger value="profile">Profile</TabsTrigger>
         <TabsTrigger value="notification">Notifications</TabsTrigger>
-        <TabsTrigger value="security">Security</TabsTrigger>
+        <TabsTrigger value="security">Login Details</TabsTrigger>
       </TabsList>
       <TabsContent value="profile">
         <Card>
@@ -44,7 +63,7 @@ export default function TabsDemo() {
             
           </CardHeader>
           <CardContent className="space-y-2">
-            
+            <Notification/>
 
 
 
@@ -55,12 +74,11 @@ export default function TabsDemo() {
       <TabsContent value="security">
         <Card>
           <CardHeader>
-            <CardTitle>Security</CardTitle>
+            <CardTitle>Login Details</CardTitle>
             
           </CardHeader>
           <CardContent className="space-y-2">
-            
-
+            <Logindetails/>
 
 
 
@@ -69,5 +87,11 @@ export default function TabsDemo() {
         </Card>
       </TabsContent>
     </Tabs>
+       
+        </div>
+      </main>
+    </div>
+  </div>
+    
   )
 }
