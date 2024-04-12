0c839939705e2c8e738c37cd2bd65d0fbe80ee98
sasandamanahara <sasandamanahara12@gmail.com>
2024-04-12 21:08:09 +0530

    button linked

--- src/app/(company)/_components/sidebar.jsx
+++ src/app/(company)/_components/sidebar.jsx
@@ -84,7 +84,7 @@ export default function SideBar() {
               
 
               <Link
-                href="#"
+                href="/companysettings"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Settings className="h-4 w-4" />
