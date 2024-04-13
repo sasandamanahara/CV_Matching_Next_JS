bc01b9f7d06315ef530e899f0b85260044b402a6
sasandamanahara <sasandamanahara12@gmail.com>
2024-04-13 20:05:19 +0530

    v1

--- src/app/(company)/allapplicants/page.jsx
+++ src/app/(company)/allapplicants/page.jsx
@@ -15,9 +15,8 @@ export default function DashBoard() {
           <div className="flex items-center">
             <h1 className="text-lg font-semibold md:text-2xl">All Applicants</h1>
           </div>
-          <div className="flex flex-3 items-left justify-center rounded-lg border border-dashed shadow-sm">
-            
-              
+          <div className="flex-auto items-left justify-center rounded-lg border border-dashed shadow-sm">
+
               <Appicantlist/>
             
           </div>
