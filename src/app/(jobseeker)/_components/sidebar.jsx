8e73ce0520f69a5c84464e96550de593e10e0f3b
Nipunikumudika <nipunikumudika@gmail.com>
2024-05-22 20:28:03 +0530

    get applied details

--- src/app/(jobseeker)/_components/sidebar.jsx
+++ src/app/(jobseeker)/_components/sidebar.jsx
@@ -65,7 +65,7 @@ export default function SideBar() {
                 Job Seeker Profile{" "}
               </Link>
               <Link
-                href="#"
+                href="/jobseekermyapplication"
                 className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
               >
                 <Users className="h-4 w-4" />
---
+++ src/app/api/getAppliedJobs/route.js
@@ -0,0 +1,34 @@
+import ApplyJob from '../../models/ApplyJob';
+
+import ConnectDB from '../../DB/connectDB';
+import { NextResponse } from 'next/server';
+
+export async function GET(req, res) {
+    await ConnectDB();
+    const { method } = req;
+    switch (method) {
+        case 'GET':
+            return await getAppliedJobs(req, res);
+        default:
+            return NextResponse.json({ success: false, message: "Invalid Request" });
+    }
+}
+
+
+
+const getAppliedJobs =  async (req, res) => {
+    await ConnectDB();
+    const userId = req.url.split('?')[1].split('=')[1];
+    console.log("ID:", userId);
+
+    if(!userId) return NextResponse.json({ success: false, message: "Please Login First"});
+
+    try {
+        console.log("trying");
+        const gettingAppliedJobs  = await ApplyJob.find({userJobSeeker : userId}).populate('userJobSeeker').populate('jobId');
+        return NextResponse.json({ success: true, data: gettingAppliedJobs });
+    } catch (error) {
+        console.log('Error in getting applied  job (server) => ', error);
+        return NextResponse.json({ success: false, message: "Something Went Wrong Please Retry login  ! "});
+    }
+}
