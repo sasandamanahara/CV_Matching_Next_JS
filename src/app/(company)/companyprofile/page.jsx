b8e81d93ad87a4c4dcd6de7ff4a1f778f67ad6a2
Sanduni Manjula <sandunimanju.27@gmail.com>
2024-05-30 23:59:38 +0530

    allapplicants serach

--- src/app/(company)/companyprofile/page.jsx
+++ src/app/(company)/companyprofile/page.jsx
@@ -16,7 +16,7 @@ export default function CompanyProfile() {
         <CompanyHeader/>
         <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl">CompanyProfile</h1>
+            <h1 className="text-lg font-semibold md:text-2xl">Company Profile</h1>
           </div>
           <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
             <div className="flex flex-col items-center gap-1 text-center">
